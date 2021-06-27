39c22a90742eead3a6edbc916e63d0dbb2cca450
richie0226 <970071@stu.kjes.tp.edu.tw>
2021-06-28 00:36:56 +0800

    Upgraded

--- src/main/java/com/fju/Title.java
+++ src/main/java/com/fju/Title.java
@@ -4,23 +4,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class AAA {
+public class Title {
     public static void main(String[] args) {
         try {
-            FileReader fileReader = new FileReader("A.txt");
+            FileReader fileReader = new FileReader("Title.txt");
             int data = fileReader.read();
-            while (data != -1) {
-                System.out.println((char) data);
+            while (data!= -1) {
+                System.out.print((char)data);
                 data = fileReader.read();
             }
             fileReader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException) {
+        } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
 
 }
-}
+
